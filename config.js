ba12921d4dd281bca89806155f2b0afad8268023
burak.genc <mrburakgenc@hotmail.com>
2024-05-20 10:40:53 +0300

    first commit

---
+++ config.js
@@ -0,0 +1,15 @@
+const config = {
+    db: {
+        host: "localhost",
+        user: "root",
+        password: "<dbPass>",
+        database: "blogdb"
+    },
+    email: {
+        username: "<mail>",
+        password: "<Pass>",
+        from: "<mail>",
+    }
+}
+
+module.exports = config;
